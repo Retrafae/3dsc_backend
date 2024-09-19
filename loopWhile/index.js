649180f7051f7ce1780a9fc8f4cb118d7e46dd19
Retrafae <130366200+Retrafae@users.noreply.github.com>
2024-09-19 08:11:20 -0300

    Add files via upload

---
+++ loopWhile/index.js
@@ -0,0 +1,23 @@
+// let n = 1
+// let n2 = Math.random()
+// // while(n <= 10){
+// //   console.log(n)
+// //   n++
+// //   //ctrl c para interromper
+// // }
+
+// // while (true) {
+// //   let n2 = (Math.random() * 60).toFixed(2)
+// //   console.log(n2)
+// // }
+
+// do {
+// console.log("-------Menu--------")
+// console.log("Qual o seu nome? ")
+// console.log("--------------------")
+
+//  n ++
+// } while (n < 10); 
+
+DESFIO 01
+1)
