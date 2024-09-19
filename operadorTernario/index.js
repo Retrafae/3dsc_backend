fd12b2018457725879672016f0cbc98f2fabd9c4
Retrafae <130366200+Retrafae@users.noreply.github.com>
2024-09-19 08:11:53 -0300

    Add files via upload

---
+++ operadorTernario/index.js
@@ -0,0 +1,10 @@
+const idade = 18;
+
+if (idade >= 18) {
+  console.log("PODE ENTRAR");
+}else{
+  console.log("ACESSO NEGADO");
+}
+
+const passe = idade >= 18 ? "PODE ENTRAR" : "ACESSO NEGADO"
+console.log(passe)
